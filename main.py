8ad73205e4278e95a5d6f72d7b6fb469579b455f
JDBryan <47214792+JDBryan@users.noreply.github.com>
2022-09-25 15:51:01 -0700

    Changed add function to times function

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
-def add(num_a, num_b):
-    return num_a + num_b
+def times(num_a, num_b):
+    return num_a * num_b
 
 def addition(num_a, num_b, num_c):
     return num_a + num_b + num_c
 
-print(add(3, 4))
-print(add(100, 43))
+print(times(3, 4))
+print(times(100, 43))
 print(addition(1, 2, 3))
