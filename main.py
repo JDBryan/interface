fcf4e9abf2f079c9566036a2078b7327c8238879
Mac Gulland <mac_gulland@yahoo.com>
2022-09-26 00:18:45 +0100

    timesed it by three. lol

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 def subtract(num_a, num_b):
     return num_a - num_b
 
-def times(num_a, num_b):
-    return num_a * num_b
+def times(num_a, num_b, num_c):
+    return num_a * num_b * num_c
 
 def addition(num_a, num_b, num_c):
     return num_a + num_b + num_c
@@ -11,7 +11,7 @@ def addition(num_a, num_b, num_c):
 print(subtract(3, 4))
 print(subtract(100, 43))
 
-print(times(3, 4))
-print(times(100, 43))
+print(times(3, 4, 10))
+print(times(100, 43, 2))
 
 print(addition(1, 2, 3))
