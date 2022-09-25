e17033c6780dc8ca6ff0e8ae94290a84a434dd7a
Mac Gulland <mac_gulland@yahoo.com>
2022-09-25 23:39:43 +0100

    added three whole numbers, how clever am i

--- main.py
+++ main.py
@@ -2,5 +2,9 @@
 def add(num_a, num_b):
     return num_a + num_b
 
+def addition(num_a, num_b, num_c):
+    return num_a + num_b + num_c
+
 print(add(3, 4))
 print(add(100, 43))
+print(addition(1, 2, 3))
