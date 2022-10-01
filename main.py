f9d88aefa9de07a2420b9be67ab12c45e86c77db
JDBryan <47214792+JDBryan@users.noreply.github.com>
2022-10-01 09:49:51 -0700

    Added isTwoOrThree function

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ def times(num_a, num_b):
 def addition(num_a, num_b, num_c):
     return num_a + num_b + num_c
 
-print(subtract(3, 4))
-print(subtract(100, 43))
+def isTwoOrThree(num):
+    if num == 2:
+        print("Number is 2, YAY!")
+    elif num == 3:
+        print("Number is 3, YAY!")
+    else:
+        print("Number is not 2 or 3 :(")
 
-print(times(3, 4))
-print(times(100, 43))
 
-print(addition(1, 2, 3))
+isTwoOrThree(addition(1, 1, 1))
+isTwoOrThree(2)
+isTwoOrThree(4)
