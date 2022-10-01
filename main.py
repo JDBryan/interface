cf838730b55ab5d660c30bdcfb533c67465ed49b
JDBryan <47214792+JDBryan@users.noreply.github.com>
2022-10-01 10:04:02 -0700

    solving merge conflict

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ def times(num_a, num_b, num_c):
 def addition(num_a, num_b, num_c):
     return num_a + num_b + num_c
 
-<<<<<<< HEAD
+def divide(num_a, num_b):
+    return num_a / num_b
+
 def isTwoOrThree(num):
     if num == 2:
         print("Number is 2, YAY!")
@@ -21,18 +23,5 @@ def isTwoOrThree(num):
 isTwoOrThree(addition(1, 1, 1))
 isTwoOrThree(2)
 isTwoOrThree(4)
-=======
-def divide(num_a, num_b):
-    return num_a / num_b
 
-print(subtract(3, 4))
-print(subtract(100, 43))
 
-print(times(3, 4, 10))
-print(times(100, 43, 2))
-
-print(addition(1, 2, 3))
-
-print(divide(100, 10))
-print(divide(16, 4))
->>>>>>> 349cdb66b9b18cb0873d90248bbdcfbc680566b3
